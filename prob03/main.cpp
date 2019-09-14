1fad32a167d741b07a9cf1f15381c18b7f46f1d0
enriquegc2001 <enriquegc2001@csu.fullerton.edu>
2019-09-14 14:45:05 -0700

    showed 2018 sales and 2019's predicitons

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,17 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+
+  int y2018 = 2103419277;
+  int y2019 = y2018 * .18;
+
+  //this will display 2018's Sales
+  std::cout << "Last year's sales were $" << y2018 << std::endl;
+
+  //this will display 2019's prediction
+  std::cout << "This year's sales prediction: " <<y2019 << std::endl;
+  return 0;
+}
